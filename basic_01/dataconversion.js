01ca9251e7f3fe7f99debc72f583ee6dbbf6fd45
Ruchi Yadav <145835786+ruchiyadav00@users.noreply.github.com>
2024-09-22 18:07:48 +0000

    opreator

--- basic_01/dataconversion.js
+++ basic_01/dataconversion.js
@@ -13,5 +13,12 @@ let isValueBoolean = Boolean(isLoggedIn);
 // for string
 const stringValue = true ;
 let StringConversion = String(stringValue);
-console.log(typeof StringConversion + " ,the value = " + StringConversion );
+// console.log(typeof StringConversion + " ,the value = " + StringConversion );
 
+// comparsion & operators
+
+// operators-  + - * / ** % 
+console.log( "1" + 2 +2);
+console.log( 1 + 2 + '2');
+
+// this happen because of js rule first priority -- if first is string ll other value treated as string & if first is number so the operation take place upto the value with string datatype
