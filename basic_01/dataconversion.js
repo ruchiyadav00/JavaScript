8ecc4ab7212e36571791e16ca9afa2e825f99d89
Ruchi Yadav <145835786+ruchiyadav00@users.noreply.github.com>
2024-09-22 17:40:29 +0000

    data conversion

---
+++ basic_01/dataconversion.js
@@ -0,0 +1,17 @@
+// for number
+const testImage = true ;
+// console.log(typeof testImage);
+let numberConversion = Number(testImage);
+// console.log(typeof numberConversion);
+// console.log( numberConversion);
+
+// for boolean
+const isLoggedIn = " " ;
+let isValueBoolean = Boolean(isLoggedIn);
+// console.log(isValueBoolean);
+
+// for string
+const stringValue = true ;
+let StringConversion = String(stringValue);
+console.log(typeof StringConversion + " ,the value = " + StringConversion );
+
